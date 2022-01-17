d7b953599708a5012f9da7204b6da807741dd733
devmichaelheming <devmichael.heming@gmail.com>
2022-01-17 17:37:36 -0400

    first commit

---
+++ src/repositories/UserRepositories.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+
+@EntityRepository(User)
+class userRepositories extends Repository<User> {
+    
+}
+
+export { userRepositories }
